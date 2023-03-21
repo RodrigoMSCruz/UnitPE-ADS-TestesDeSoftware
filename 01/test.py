cd11069ff3d5a66b14a191629e6fee99e04dcc60
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-03-21 15:43:09 -0300

    Inclusões de mais de mais testes em 01/test.py

--- 01/test.py
+++ 01/test.py
@@ -3,3 +3,7 @@ from main import calcula_par
 
 def test_par():
   assert calcula_par(20) is True
+  assert calcula_par(3) is False
+  assert calcula_par(0) is True
+  assert calcula_par(-20) is True
+  assert calcula_par(101) is False
