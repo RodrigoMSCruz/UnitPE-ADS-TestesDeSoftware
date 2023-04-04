b67e618b8e75aaf52f9c2b44b0213812d840bc32
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-04-04 09:51:49 -0300

    Exercícios de reforço em python.

---
+++ 02/teste03.py
@@ -0,0 +1,16 @@
+lista = []
+maior = menor = 0
+quant = int(input("Digite a quantidade de números a ser digitada: "))
+for i in range(0, quant):
+    numero = int(input("Digite o número: "))
+    lista.append(numero)
+    if i == 0:
+        maior = menor = numero;
+    elif numero > maior:
+        maior = numero
+    elif numero < menor:
+        menor = numero
+        
+print("O maior número é: ", maior)
+print("O menor número é: ", menor)
+
---
+++ 02/testes02.py
@@ -0,0 +1,10 @@
+
+for x in range(0,11):
+    print(x)
+    
+lista = [0,1,2,3,4,5,6]
+for i in lista:
+    if i%2 == 0:
+        print(i, "é par.")
+    else:
+        print(i, " é ímpar.")
