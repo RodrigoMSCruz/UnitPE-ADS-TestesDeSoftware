7db96387ffa35588f1cc027d48f3ac9810defc34
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-04-04 09:58:08 -0300

    Correções pequenas em 02.

--- 02/teste03.py
+++ 02/teste03.py
@@ -1,11 +1,12 @@
 lista = []
 maior = menor = 0
 quant = int(input("Digite a quantidade de números a ser digitada: "))
+
 for i in range(0, quant):
     numero = int(input("Digite o número: "))
     lista.append(numero)
     if i == 0:
-        maior = menor = numero;
+        maior = menor = numero
     elif numero > maior:
         maior = numero
     elif numero < menor:
