80dfd462e333fd1e3c6444e85291438558c91f74
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-04-11 09:51:49 -0300

    Create L01-Ex06.py

---
+++ 04-ExercicosComStrings/L01-Ex06.py
@@ -0,0 +1,33 @@
+
+def DataPorExtenso(data):
+    mes = ''
+    if data[3:5] == '01':
+        mes = ' de Janeiro'
+    if data[3:5] == '02':
+        mes = ' de Fevereiro'
+    if data[3:5] == '03':
+        mes = ' de Março'
+    if data[3:5] == '04':
+        mes = ' de Abril'
+    if data[3:5] == '05':
+        mes = ' de Maio'
+    if data[3:5] == '06':
+        mes = ' de Junho'
+    if data[3:5] == '07':
+        mes = ' de Julho'
+    if data[3:5] == '08':
+        mes = ' de Agosto'
+    if data[3:5] == '09':
+        mes = ' de Setembro'
+    if data[3:5] == '10':
+        mes = ' de Outubro'
+    if data[3:5] == '11':
+        mes = ' de Novembro'
+    if data[3:5] == '12':
+        mes = ' de Dezembro'
+    #
+    print('Você nasceu em ', data[0:2], mes, ' de ', data[6:10])
+#
+
+nascimento = input('Digite sua data de nascimento: (DD/MM/AAAA)')
+DataPorExtenso(nascimento)
