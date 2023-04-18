b6abd56c027264be08f9775b0b6b6c4b3eb79414
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-04-17 22:47:52 -0300

    Create test02.py

---
+++ 06-Prova01/test02.py
@@ -0,0 +1,10 @@
+from main01 import existe_string
+
+def test_existe_string():
+  assert existe_string("joao", "joao e o pé de feijão") == True
+  assert existe_string("maria", "joao e o pé de feijão") == False
+  assert existe_string("eij", "joao e o pé de feijão") == True
+  assert existe_string('Rodrigo', 'João e o pé de mandioca') == False
+  assert existe_string('Testes','Q.A.') == False
+  assert existe_string('Teste','Disciplina de Testes') == True
+#
